b4f17e3a60277072e7ecd8cf330f75f037f7973c
PakoBl <50603188+PakoBl@users.noreply.github.com>
2019-07-03 23:05:01 -0500

    Add files via upload

---
+++ Actividad 2/PD/Creacionales/Builder/Shop.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    /// <summary>
+
+    /// The 'Director' class
+
+    /// </summary>
+
+    class Shop
+
+    {
+        // Builder uses a complex series of steps
+
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+    /// <summary>
+
+    /// The 'Builder' abstract class
+
+    /// </summary>
+
+    abstract class VehicleBuilder
+
+    {
+        protected Vehicle vehicle;
+
+        // Gets vehicle instance
+
+        public Vehicle Vehicle
+        {
+            get { return vehicle; }
+        }
+
+        // Abstract build methods
+
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+}
---
+++ Actividad 2/PD/Estructurales/Adapter/MainApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    /// <summary>
+
+    /// MainApp startup class for Real-World 
+
+    /// Adapter Design Pattern.
+
+    /// </summary>
+
+    class MainApp
+
+    {
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        static void Main()
+        {
+            // Non-adapted chemical compound
+
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
